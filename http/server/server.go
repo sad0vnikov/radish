149417b174cda90cffea0ff85aca311cfea3360a
Alexander Sadovnikov <alex@sadovnikov.net>
2017-03-25 21:33:24 +0300

    + http requests logging

--- http/server/server.go
+++ http/server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/sad0vnikov/radish/logger"
 )
 
 //HTTPServer server configuration
@@ -12,15 +15,19 @@ type HTTPServer struct {
 
 type handler func(w http.ResponseWriter, r *http.Request)
 
+var router = mux.NewRouter()
+
 // Init starts http server
 func (server HTTPServer) Init() {
-	err := http.ListenAndServe(":8080", nil)
+	loggingRouter := handlers.LoggingHandler(logger.GetOutput(), router)
+	err := http.ListenAndServe(":8080", loggingRouter)
 	if err != nil {
-		log.Fatal(err)
+		logger.Critical(err)
 	}
 }
 
 //AddHandler adds a http handler
 func (server HTTPServer) AddHandler(method, path string, h handler) {
-	http.HandleFunc("/api/"+path, h)
+	router.HandleFunc("/api/"+path, h).
+		Methods(method)
 }
