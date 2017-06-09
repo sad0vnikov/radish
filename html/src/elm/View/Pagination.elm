eb955279f47da58f339de5363341a3b13d0c890f
Alexander Sadovnikov <alex@sadovnikov.net>
2017-06-09 22:21:41 +0300

    * limiting paginator buttons count

--- html/src/elm/View/Pagination.elm
+++ html/src/elm/View/Pagination.elm
@@ -12,13 +12,73 @@ drawPager pagesCount curPage onClickMsg =
     nav [] [
         ul [class "pagination"] (
             (range 1 pagesCount) 
+                |> filterPageButtons pagesCount curPage
+                |> addFirstAndLastPages pagesCount 
                 |> List.map (\pageNum -> 
-                    li (markPageIfActive pageNum curPage) [
-                        a [href "#", onClick (onClickMsg pageNum)] [text (toString pageNum)]
-                    ]
+                    if pageNum > 0 then
+                        li (markPageIfActive pageNum curPage) [
+                            a [href "#", onClick (onClickMsg pageNum)] [text (toString pageNum)]
+                        ]
+                    else
+                        li [class "disabled"] [
+                            a [href "#"] [text "..."]
+                        ]
                 )
         )
     ]
+
+
+addFirstAndLastPages : Int -> (List Int) -> (List Int)
+addFirstAndLastPages lastPagesNum pages =
+    (getFirstPageNumbers pages) ++ pages ++ (getLastPageNumbers lastPagesNum pages)
+
+{-- a button for page 0 will be displayed button with ellipsis --}
+getFirstPageNumbers : (List Int) -> (List Int)
+getFirstPageNumbers pages =
+    if List.head pages == Just 2 then
+        [1]
+    else
+        [1, 0]
+
+{-- a button for page 0 will be displayed button with ellipsis --}
+getLastPageNumbers : Int -> (List Int) -> (List Int)
+getLastPageNumbers lastPageNum pages =
+    if List.head (List.reverse pages) == Just (lastPageNum - 1) then
+        [lastPageNum]
+    else
+        [0, lastPageNum]
+
+filterPageButtons :  Int -> Int -> (List Int) -> (List Int)
+filterPageButtons totalPagesCount currentPage pages =
+    let
+      pagerLinksToDrawCount = 7
+      chosenPageNbhd = getChosenPageNeighbourhood totalPagesCount pagerLinksToDrawCount currentPage
+    in
+      List.filter (filterButton chosenPageNbhd) pages
+    
+filterButton : (Int, Int) -> Int -> Bool
+filterButton  (chosenPageNbhdLeftEdge, chosenPageNbhdRightEdge) pageToFilterNum =
+    if pageToFilterNum >= chosenPageNbhdLeftEdge && pageToFilterNum <= chosenPageNbhdRightEdge then
+        True
+    else
+        False
+
+getChosenPageNeighbourhood : Int -> Int -> Int -> (Int, Int)
+getChosenPageNeighbourhood totalPagesCount pagesToDrawCount chosenPageNum  =
+    let 
+        curPageNbhdLeftHalfSize = (pagesToDrawCount - 2) // 2
+        curPageNbhdRightHalfSize = pagesToDrawCount - curPageNbhdLeftHalfSize - 2
+    in
+        if totalPagesCount <= pagesToDrawCount then
+            (2, pagesToDrawCount - 1)
+        else if chosenPageNum - curPageNbhdLeftHalfSize <= 1 then
+            (2, curPageNbhdRightHalfSize + curPageNbhdLeftHalfSize + 1)
+        else if chosenPageNum + curPageNbhdRightHalfSize >= totalPagesCount then
+            (totalPagesCount - curPageNbhdLeftHalfSize - curPageNbhdRightHalfSize, totalPagesCount - 1)
+        else
+            (chosenPageNum  - curPageNbhdLeftHalfSize, chosenPageNum + curPageNbhdRightHalfSize - 1)
+        
+
 
 markPageIfActive : Int -> Int -> List (Html.Attribute msg)
 markPageIfActive pageNum chosenPage =
