1aa21395899bbb0e5c8f2bc8e3ffdd4e3fc276f2
Alexander Sadovnikov <alex@sadovnikov.net>
2017-08-29 00:18:41 +0300

    * broken tests

--- redis/db/connections_test.go
+++ redis/db/connections_test.go
@@ -3,11 +3,12 @@ package db
 import (
 	"reflect"
 	"testing"
+	"github.com/sad0vnikov/radish/redis"
 )
 
 func TestCorrectStringParses(t *testing.T) {
 	s := "db0:keys=5,expires=0,avg_ttl=0"
-	expected := ServerKeyspaceStat{KeysCount: 5}
+	expected := redis.ServerKeyspaceStat{KeysCount: 5}
 
 	result := parseKeyspaceStatString(s)
 	eq := reflect.DeepEqual(result, expected)
